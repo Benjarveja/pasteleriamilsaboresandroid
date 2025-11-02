1acb91f946a52b0393ab16c041577144c6f3de0f
BenjaValde <benj.valderrama@duocuc.cl>
2025-11-02 00:06:54 -0300

    Modificacion en la seccion de inicio, se agregan imagenes al carrusel de fotos

--- app/src/main/java/com/example/pasteleriamilssaboresandroid/data/HomeData.kt
+++ app/src/main/java/com/example/pasteleriamilssaboresandroid/data/HomeData.kt
@@ -3,7 +3,7 @@ package com.example.pasteleriamilssaboresandroid.data
 data class ImageSliderItem(val image: String, val title: String, val subtitle: String)
 
 val imageSliderItems = listOf(
-    ImageSliderItem("images/products/TC001.jpg", "Torta de Chocolate", "La mejor torta de chocolate del mundo"),
-    ImageSliderItem("images/products/PT001.jpg", "Pie de Limón", "El equilibrio perfecto entre dulce y ácido"),
-    ImageSliderItem("images/products/PV001.jpg", "Torta de Vainilla", "Un clásico que nunca falla"),
+    ImageSliderItem("images/noticia50anos.jpg", "50 Años de Dulce Tradición", "Acompañándote en tus celebraciones más importantes."),
+    ImageSliderItem("images/noticiadiaadia.jpg", "Nuevos Sabores Cada Día", "Descubre nuestras últimas creaciones y déjate sorprender."),
+    ImageSliderItem("images/noticiaguinness.jpg", "Una Historia de Récord", "Orgullosos de nuestra participación en el Récord Guinness de 1995.")
 )
